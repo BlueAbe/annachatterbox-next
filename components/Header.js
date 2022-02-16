5e24f267bca283e910547a5b224f4f02b2c86df6
BlueAbe <andrzej.jan.jaworski@gmail.com>
2022-02-16 11:31:17 +0100

    correct flags load in header

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-export default function Header({ propsFlags }) {
+export default function Header() {
   // STATE
   const [flags, setFlags] = useState([]);
   const [restFlags, setRestFlags] = useState([]);
@@ -28,42 +28,16 @@ export default function Header({ propsFlags }) {
     return array;
   }
 
-  // // GENERATE FLAGS AFTER PAGE LOAD - FROM HEROKU
-  useEffect(async function () {
-    const res = await fetch(
-      " https://annachatterbox.herokuapp.com/api/flags?populate=*"
-    );
-    const strapi = await res.json();
-    const flagObjectsTab = strapi.data[0].attributes.pictures.data;
-    const flagUrlsTab = flagObjectsTab.map((el) => {
-      return el.attributes.url;
-    });
-    const randomFlagsTab = shuffle(flagUrlsTab);
-    let viewportWidth = window.innerWidth;
-    const changeFlagsDetails1 = (i) => {
-      const l = randomFlagsTab.length;
-      const restFlagsTable = randomFlagsTab.slice(0, l - i);
-      const sliceRandomFlagsTab = randomFlagsTab.slice(-i);
-      setFlags(sliceRandomFlagsTab);
-      setRestFlags(restFlagsTable);
-    };
-    if (viewportWidth > 900) {
-      changeFlagsDetails1(50);
-    }
-    if (viewportWidth <= 900 && viewportWidth > 600) {
-      changeFlagsDetails1(40);
-    }
-    if (viewportWidth <= 600) {
-      changeFlagsDetails1(25);
-    }
-  }, []);
-
-  // GENERATE FLAGS AFTER PAGE LOAD - FROM FILES
+  // GENERATE FLAGS AFTER PAGE LOAD - FROM HEROKU
   // useEffect(async function () {
-  //   const flagUrlsTab = propsFlags.map((el) => {
-  //     return `/flags/${el}`;
+  //   const res = await fetch(
+  //     " https://annachatterbox.herokuapp.com/api/flags?populate=*"
+  //   );
+  //   const strapi = await res.json();
+  //   const flagObjectsTab = strapi.data[0].attributes.pictures.data;
+  //   const flagUrlsTab = flagObjectsTab.map((el) => {
+  //     return el.attributes.url;
   //   });
-  //   // console.log(flagUrlsTab);
   //   const randomFlagsTab = shuffle(flagUrlsTab);
   //   let viewportWidth = window.innerWidth;
   //   const changeFlagsDetails1 = (i) => {
@@ -83,6 +57,92 @@ export default function Header({ propsFlags }) {
   //     changeFlagsDetails1(25);
   //   }
   // }, []);
+
+  // GENERATE FLAGS AFTER PAGE LOAD - FROM FILES
+  useEffect(async function () {
+    const propsFlags = [
+      "tn_ac-flag.gif",
+      "tn_as-flag.gif",
+      "tn_bb-flag.gif",
+      "tn_bc-flag.gif",
+      "tn_bf-flag.gif",
+      "tn_bh-flag.gif",
+      "tn_bp-flag.gif",
+      "tn_bx-flag.gif",
+      "tn_by-flag.gif",
+      "tn_ca-flag.gif",
+      "tn_cm-flag.gif",
+      "tn_do-flag.gif",
+      "tn_ei-flag.gif",
+      "tn_fj-flag.gif",
+      "tn_fm-flag.gif",
+      "tn_ga-flag.gif",
+      "tn_gh-flag.gif",
+      "tn_gj-flag.gif",
+      "tn_gv-flag.gif",
+      "tn_in-flag.gif",
+      "tn_jm-flag.gif",
+      "tn_ke-flag.gif",
+      "tn_kr-flag.gif",
+      "tn_li-flag.gif",
+      "tn_lt-flag.gif",
+      "tn_mi-flag.gif",
+      "tn_mr-flag.gif",
+      "tn_mt-flag.gif",
+      "tn_nh-flag.gif",
+      "tn_ni-flag.gif",
+      "tn_nr-flag.gif",
+      "tn_nz-flag.gif",
+      "tn_od-flag.gif",
+      "tn_pk-flag.gif",
+      "tn_pp-flag.gif",
+      "tn_ps-flag.gif",
+      "tn_rm-flag.gif",
+      "tn_rp-flag.gif",
+      "tn_rw-flag.gif",
+      "tn_sc-flag.gif",
+      "tn_se-flag.gif",
+      "tn_sf-flag.gif",
+      "tn_sl-flag.gif",
+      "tn_sn-flag.gif",
+      "tn_st-flag.gif",
+      "tn_su-flag.gif",
+      "tn_td-flag.gif",
+      "tn_tn-flag.gif",
+      "tn_tv-flag.gif",
+      "tn_tz-flag.gif",
+      "tn_ug-flag.gif",
+      "tn_uk-flag.gif",
+      "tn_us-flag.gif",
+      "tn_wa-flag.gif",
+      "tn_ws-flag.gif",
+      "tn_wz-flag.gif",
+      "tn_za-flag.gif",
+      "tn_zi-flag.gif",
+    ];
+    const flagUrlsTab = propsFlags.map((el) => {
+      return `/flags/${el}`;
+    });
+    // console.log(flagUrlsTab);
+    const randomFlagsTab = shuffle(flagUrlsTab);
+    let viewportWidth = window.innerWidth;
+    const changeFlagsDetails1 = (i) => {
+      const l = randomFlagsTab.length;
+      const restFlagsTable = randomFlagsTab.slice(0, l - i);
+      const sliceRandomFlagsTab = randomFlagsTab.slice(-i);
+      setFlags(sliceRandomFlagsTab);
+      setRestFlags(restFlagsTable);
+    };
+    if (viewportWidth > 900) {
+      changeFlagsDetails1(50);
+    }
+    if (viewportWidth <= 900 && viewportWidth > 600) {
+      changeFlagsDetails1(40);
+    }
+    if (viewportWidth <= 600) {
+      changeFlagsDetails1(25);
+    }
+  }, []);
 
   // CHANGE FLAGS AFTER CLICK
   const changeFlagsDetails2 = (i) => {
